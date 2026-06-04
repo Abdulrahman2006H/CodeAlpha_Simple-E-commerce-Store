bccc3c282ffd49c5fb86d124b3180031bb17b696
Abdulrahman2006H <Abdulrahman.Hossam2006@gmail.com>
2026-06-04 14:49:12 +0300

    Add navbar component

---
+++ Frontend/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, computed, inject } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  private readonly cartService = inject(CartService);
+  private readonly authService = inject(AuthService);
+
+  readonly cartCount = computed(() => this.cartService.count());
+  readonly user = this.authService.user;
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
