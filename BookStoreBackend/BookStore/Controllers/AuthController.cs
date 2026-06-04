75155a03cc56696975be648970cded929b7033ca
Abdulrahman2006H <Abdulrahman.Hossam2006@gmail.com>
2026-06-04 19:40:22 +0300

    Add BooksController simple authentication APIs

---
+++ BookStoreBackend/BookStore/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using BookStore.Api.DTOs;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly BookStoreDbContext _context;
+
+    public AuthController(BookStoreDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register(RegisterDto dto)
+    {
+        if (await _context.Users.AnyAsync(u => u.Email == dto.Email))
+            return BadRequest("Email already exists");
+
+        var user = new User
+        {
+            FullName = dto.FullName,
+            Email = dto.Email,
+            PasswordHash = dto.Password 
+        };
+
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return Ok(new
+        {
+            message = "Registered successfully",
+            user = new { user.Id, user.FullName, user.Email, user.PasswordHash }
+        });
+    }
+
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login(LoginDto dto)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == dto.Email);
+        if (user == null)
+            return Unauthorized("Invalid email or password");
+
+        if (user.PasswordHash != dto.Password) 
+            return Unauthorized("Invalid email or password");
+
+        return Ok(new
+        {
+            message = "Login successful",
+            user = new { user.Id, user.FullName, user.Email, user.PasswordHash }
+        });
+    }
+}
--- BookStoreBackend/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
+++ BookStoreBackend/BookStore/obj/Debug/net8.0/BookStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+621099f98f758db6ba563b8b3008d84f33545b1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+219c6a3ed242304febf2c3194df2d1f15611cbe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
