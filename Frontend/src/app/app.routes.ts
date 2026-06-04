a6a8795a0e94774a91040ce19f747b4578fc3e77
Abdulrahman2006H <Abdulrahman.Hossam2006@gmail.com>
2026-06-04 13:49:34 +0300

    Connect frontend components with app routing

---
+++ Frontend/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { CartComponent } from './pages/cart/cart.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '' }
+];
